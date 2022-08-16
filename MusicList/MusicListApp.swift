603eb99f08d0e744fde17240f704f5d8c8e246ce
Steven Wijaya <steven.wijaya2001@gmail.com>
2022-08-16 17:40:41 +0700

    Initial Commit

---
+++ MusicList/MusicListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicListApp.swift
+//  MusicList
+//
+//  Created by Steven Wijaya on 16.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct MusicListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
